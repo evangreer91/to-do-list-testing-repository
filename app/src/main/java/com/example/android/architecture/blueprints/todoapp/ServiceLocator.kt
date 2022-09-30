fc924ad74573a26eea13a00cab3fd8aacaa52acf
Evan Greer <evan.greer@iterable.com>
2022-09-30 15:41:58 -0600

    uses the service locator to create a fake repository and connects this repository to our integration tests

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/ServiceLocator.kt
@@ -1,6 +1,7 @@
 package com.example.android.architecture.blueprints.todoapp
 
 import android.content.Context
+import androidx.annotation.VisibleForTesting
 import androidx.room.Room
 import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
 import com.example.android.architecture.blueprints.todoapp.data.source.TasksDataSource
@@ -8,15 +9,23 @@ import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepo
 import com.example.android.architecture.blueprints.todoapp.data.source.local.TasksLocalDataSource
 import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
 import com.example.android.architecture.blueprints.todoapp.data.source.remote.TasksRemoteDataSource
+import kotlinx.coroutines.runBlocking
 
 // Service locator is a singleton
 object ServiceLocator {
 
+    private val lock = Any()
+
     private var database: ToDoDatabase? = null
 
     // annotated with @Volatile - repository could be used and requested by multiple threads
+    // annotation @VisibleForTesting expresses that this method is public for testing purposes only
+
+    // One of the downsides of using a service locator is that it is a shared singleton
+    // we need to reset the state of the service locator when the test finishes and tests cannot be run in parallel
     @Volatile
     var tasksRepository: TasksRepository? = null
+        @VisibleForTesting set
 
     // returns an existing repository or make and return a new repository if needed
     fun provideTasksRepository(context: Context): TasksRepository {
@@ -49,5 +58,24 @@ object ServiceLocator {
         ).build()
         database = result
         return result
+    }
+
+    // this method clears the database, closes the database,
+    // sets the database and the repository back to null
+    // this is run in a coroutine since deleteAllTasks is a suspend function using RunBlocking
+    @VisibleForTesting
+    fun resetRepository() {
+        synchronized(lock) {
+            runBlocking {
+                TasksRemoteDataSource.deleteAllTasks()
+            }
+            // Clear all data to avoid test pollution.
+            database?.apply {
+                clearAllTables()
+                close()
+            }
+            database = null
+            tasksRepository = null
+        }
     }
 }
