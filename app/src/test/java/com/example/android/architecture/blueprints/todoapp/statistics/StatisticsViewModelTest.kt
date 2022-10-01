ea73ef4e53c7f53274209b94fc236eab6f1d9e08
Evan Greer <evan.greer@iterable.com>
2022-10-01 17:48:26 -0600

    creates loading indicator tests using pauseDispatcher and resumeDispatcher

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -3,10 +3,13 @@ package com.example.android.architecture.blueprints.todoapp.statistics
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
 import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.Matchers.`is`
 import org.junit.Rule
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class StatisticsViewModelTest {
@@ -32,5 +35,20 @@ class StatisticsViewModelTest {
         tasksRepository = FakeTestRepository()
 
         statisticsViewModel = StatisticsViewModel(tasksRepository)
+    }
+
+    @Test
+    fun loadTasks_loading() {
+
+        // Load the task in the view model.
+        mainCoroutineRule.pauseDispatcher()
+        statisticsViewModel.refresh()
+
+        // Then progress indicator is shown.
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(true))
+
+        // Then progress indicator is hidden.
+        mainCoroutineRule.resumeDispatcher()
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(false))
     }
 }
